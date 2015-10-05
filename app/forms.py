fb5ad9c6f739c09e391d12912a903eb3ad2e652f
Bryce Drew <BryceDrew.1@gmail.com>
2015-10-05 14:10:57 -0600

    Form

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,9 @@
 from flask.ext.wtf import Form
-from wtforms import StringField, BooleanField
+from wtforms import StringField, BooleanField, PasswordField
 from wtforms.validators import DataRequired
 
 
 class LoginForm(Form):
-    openid = StringField('openid', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired()])
+    password = StringField('Password', validators=[DataRequired()])
     remember_me = BooleanField('remember_me', default=False)
