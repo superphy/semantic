e2b9bd157526de6b7295cb858efeb1d1d890574b
Stephen Kan <stebokan@gmail.com>
2015-10-16 14:53:16 -0600

    Added a sample PyUnit test.

---
+++ uploader/tests/sample_tests.py
@@ -0,0 +1,30 @@
+__author__ = 'Stephen Kan'
+
+import unittest
+
+class FooTest(unittest.TestCase):
+    """Sample test case"""
+
+    # preparing to test
+    def setUp(self):
+        """ Setting up for the test """
+        print "FooTest:setUp_:begin"
+        ## do something...
+        print "FooTest:setUp_:end"
+
+    # ending the test
+    def tearDown(self):
+        """Cleaning up after the test"""
+        print "FooTest:tearDown_:begin"
+        ## do something...
+        print "FooTest:tearDown_:end"
+
+    # test routine A
+    def testA(self):
+        """Test routine A"""
+        print "FooTest:testA"
+
+    # test routine B
+    def testB(self):
+        """Test routine B"""
+        print "FooTest:testB"
