0931c810cccd7640d79a0e217a315293e92176fc
Stephen Kan <stebokan@gmail.com>
2015-10-19 13:28:31 -0600

    Added SERVER_NAME config to Flask object

--- app/__init__.py
+++ app/__init__.py
@@ -20,6 +20,7 @@ login_manager.login_view = 'auth.login'
 
 def create_app(config_name):
     app = Flask(__name__)
+    app.config['SERVER_NAME'] = 'http://localhost'
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
 
