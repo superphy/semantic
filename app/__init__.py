cef455ccab37f77abc90f5c324294454c6ee4ff6
Stephen Kan <stebokan@gmail.com>
2015-10-19 16:14:39 -0600

    Modified the Flask tests and requirements so they would pass appropriately. Some modules may need to be downgraded to properly function.

--- app/__init__.py
+++ app/__init__.py
@@ -20,7 +20,6 @@ login_manager.login_view = 'auth.login'
 
 def create_app(config_name):
     app = Flask(__name__)
-    app.config['SERVER_NAME'] = 'http://localhost'
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
 
