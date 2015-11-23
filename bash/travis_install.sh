c32ab80ca0fcc9b06e84ddb775bf39848f5cc1d8
Stephen Kan <stebokan@gmail.com>
2015-11-23 13:57:09 -0700

    Travis CI: trying to get start_blazegraph to work (reason for error: Blazegraph downloader is not triggering) (also, trying to pinpoint why multiple different paths for postactivate.sh is being added to the end of activate)

--- bash/travis_install.sh
+++ bash/travis_install.sh
@@ -18,17 +18,25 @@ fi
 
 deactivate
 
+#Setting up sqlite server for user auth
+if ! find data-dev.sqlite | read v; then
+	echo Setting up SQL server
+	./manage.py db upgrade &> /dev/null
+fi
+echo sqlite server setup complete!
+
 #Getting the graph db jar file from remote server.
-mkdir db &> /dev/null
+mkdir db
 if ! find db/bigdata-bundled.jar | read v; then
 	echo Downloading Blazegraph jar file...
 	cd db;
 	wget "http://iweb.dl.sourceforge.net/project/bigdata/bigdata/1.5.3/bigdata-bundled.jar";
 	cd ..;
 fi
+echo Blazegraph setup complete!
 
 #Getting the BLAST+ gzip file from remote server.
-mkdir blast &> /dev/null
+mkdir blast
 if ! find blast/ncbi*/ | read v; then
 	echo Downloading NCBI BLAST+ jar file...
 	cd blast;
@@ -36,20 +44,13 @@ if ! find blast/ncbi*/ | read v; then
 	tar zxvpf *x64-linux.tar.gz
 	cd ..;
 fi
-
-#Setting up sqlite server for user auth
-if ! find data-dev.sqlite | read v; then
-	echo Setting up SQL server
-	./manage.py db upgrade &> /dev/null
-fi
+echo BLAST+ setup complete!
 
 # Add environment setup scripts to activate
 BASHDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo Adding postactivate script to venv/bin/activate
 echo "# Superphy Environment setup" >> venv/bin/activate
 echo "source $BASHDIR/postactivate.sh" >> venv/bin/activate
-
-sleep 10
 
 #Run related scripts
 bash bash/start_blazegraph
