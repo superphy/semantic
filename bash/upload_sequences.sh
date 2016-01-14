8652edaa02b7c5e4eb399d7ee9281696f1a98587
Bryce Drew <BryceDrew.1@gmail.com>
2016-01-14 09:35:40 -0700

    Changed hardcoded reference to dev machine to relative path
    This will allow this script to be run on other machines.

--- bash/upload_sequences.sh
+++ bash/upload_sequences.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #makes a superphy namespace
-source /home/ubiquitin/PycharmProjects/semantic/venv/bin/activate
-python /home/ubiquitin/PycharmProjects/semantic/python_package/uploader/sequence_upload.py
+source="$(pwd)"/venv/bin/activate
+python "$(pwd)"/python_package/uploader/sequence_upload.py
 deactivate
 
 exit 0
