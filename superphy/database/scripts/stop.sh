08171e8dc9c088bc84bda9206650a53878f3fa19
Bryce Drew <BryceDrew.1@gmail.com>
2016-02-08 11:16:17 -0700

    Fixed stop script to stop a sudo process listening on the port we wish to use, if it is one.

--- superphy/database/scripts/stop.sh
+++ superphy/database/scripts/stop.sh
@@ -1,14 +1,14 @@
 #!bin/bash
 source scripts/config
-result=$(ps ax | grep ${PORT} | grep java | awk '{print $1}')
-while [ ! -z $result ]; do
-	result=$(ps ax | grep ${PORT} | grep java | awk '{print $1}')
-	if [ ! -z "$result" ]; then
+result="$(ps ax | grep ${PORT} | grep java | awk '{print $1}') $(lsof -t -i:$PORT)" && echo $result
+while [ "$result" != " " ]; do
+	result="$(ps ax | grep ${PORT} | grep java | awk '{print $1}') $(lsof -t -i:$PORT)" && echo $result
+	if [ "$result" != " " ]; then
 		{
-			kill -9 $result
+			kill -9 $(echo $result | awk '{print $1}')
 		} || {
 			echo "Sudo killing existing blazegraph..."
-			sudo kill -9 $result
+			sudo kill -9 $(echo $result | awk '{print $1}')
 		}
 		echo "Blazegraph has been terminated!"
 	else
