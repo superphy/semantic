16b11f9a77841c2ebb11aaed694d8e853c001b78
Bryce Drew <BryceDrew.1@gmail.com>
2016-02-08 10:27:47 -0700

    added loop to stop script, in case there is more than one process listening to the port when we go to use it.

--- superphy/database/scripts/stop.sh
+++ superphy/database/scripts/stop.sh
@@ -1,14 +1,17 @@
 #!bin/bash
 source scripts/config
 result=$(ps ax | grep ${PORT} | grep java | awk '{print $1}')
-if [ ! -z "$result" ]; then
-	{
-		kill -9 $result
-	} || {
-		echo "Sudo killing existing blazegraph..."
-		sudo kill -9 $result
-	}
-	echo "Blazegraph has been terminated!"
-else
-	echo "No Blazeraph running on port $PORT..."
-fi
+while [ ! -z $result ]; do
+	result=$(ps ax | grep ${PORT} | grep java | awk '{print $1}')
+	if [ ! -z "$result" ]; then
+		{
+			kill -9 $result
+		} || {
+			echo "Sudo killing existing blazegraph..."
+			sudo kill -9 $result
+		}
+		echo "Blazegraph has been terminated!"
+	else
+		echo "No Blazeraph running on port $PORT..."
+	fi
+done
