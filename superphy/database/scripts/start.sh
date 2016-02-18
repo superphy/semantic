e0cda4167caf18129fed53902bf716ddae64ebd0
Bryce Drew <BryceDrew.1@gmail.com>
2016-02-18 09:49:41 -0700

    Database now has optional folders for keeping data separate from each segment
    - It is intended to have 3 different jnl files in 3 different folders:
    	development
    	production
    	testing

--- superphy/database/scripts/start.sh
+++ superphy/database/scripts/start.sh
@@ -8,10 +8,24 @@ bash scripts/stop.sh <&- 1>/dev/null
 
 echo Starting Blazegraph...
 
-
 cd blazegraph
 if find ${JARNAME} | read v; then
-	/usr/bin/java -ea -Xmx4g -server -cp ${JARNAME} com.bigdata.rdf.sail.webapp.NanoSparqlServer $PORT $NAMESPACE $PROPERTIES_FILE 2>&1 &
+	if [ $# -eq 0 ]; then
+		{
+			cd $DEFAULT_SERVER
+		} || {
+			mkdir $DEFAULT_SERVER
+			cd $DEFAULT_SERVER
+		}
+	else
+		{
+			cd $1
+		} || {
+			mkdir $1
+			cd $1
+		}
+	fi
+	/usr/bin/java -ea -Xmx4g -server -cp ../${JARNAME} com.bigdata.rdf.sail.webapp.NanoSparqlServer $PORT $NAMESPACE ../$PROPERTIES_FILE 2>&1 &
 	response=$(curl -s -i http://localhost:${PORT}/blazegraph/ | grep "200 OK")
 	while [[ $response = "" ]]; do
 		echo "Blazegraph not initialized"
