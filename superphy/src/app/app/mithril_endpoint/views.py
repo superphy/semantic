edb6acfbd6008408da2b47d13ad4e0fcc48e5f6b
Bryce Drew <BryceDrew.1@gmail.com>
2016-03-11 11:22:09 -0700

    cleaned up endpoint views.

--- superphy/src/app/app/mithril_endpoint/views.py
+++ superphy/src/app/app/mithril_endpoint/views.py
@@ -1,39 +1,39 @@
 #views.py
-from flask import render_template, redirect, url_for, abort, flash, request,\
-    current_app, make_response, Response, jsonify
+"""
+views.py
+provides the endpoints for this particular blueprint.
+"""
+from flask import jsonify
 from superphy.shared import sparql
 
 from . import mithril
 
 @mithril.after_request
 def add_header(response):
-    #Append after request the nessesary headers.
+    """
+    Append after request the nessesary headers.
+    """
     response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Access-Control-Allow-Headers'] = "accept, content-type"
     return response
 
-@mithril.route('/query', methods = ['GET'])
-def query():
-    #General query to test if the requests are working.
-    data = (sparql.get_x_triples(10))
-    return jsonify(data)
-
-@mithril.route('/meta', methods = ['GET', 'POST'])
-def meta():
-    results = (sparql.get_all_genome_metadata())
-    return jsonify(results)
-
-@mithril.route('/genomes', methods = ['GET', 'POST'])
+@mithril.route('/genomes', methods=['GET', 'POST'])
 def genomes():
+    """
+    General query that returns all genomes and their metadata.
+    """
     results = (sparql.get_all_genome_metadata())
     return jsonify(results)
 
-@mithril.route('/genome/<genomeid>', methods = ['GET', 'POST'])
+@mithril.route('/genome/<genomeid>', methods=['GET', 'POST'])
 def genome(genomeid):
+    """
+    Returns the metadata of a particular genome in json format.
+    """
     results = (sparql.get_genome_metadata(genomeid))
     return jsonify(results)
 
-@mithril.route('/genes', methods = ['GET', 'POST'])
+@mithril.route('/genes', methods=['GET', 'POST'])
 def genes():
     """
     General query that returns all genes and their metadata.
@@ -41,8 +41,7 @@ def genes():
     results = (sparql.get_all_genes())
     return jsonify(results)
 
-
-@mithril.route('/gene/<geneid>', methods = ['GET', 'POST'])
+@mithril.route('/gene/<geneid>', methods=['GET', 'POST'])
 def gene(geneid):
     """
     Returns the metadata of a particular gene in json format.
