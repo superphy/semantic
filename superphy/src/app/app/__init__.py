2d9e3a3feb0779c6d599a53e01d627c1fbe0d128
Bryce Drew <BryceDrew.1@gmail.com>
2016-03-14 14:51:06 -0600

    gutted code from flask tutorials

--- superphy/src/app/app/__init__.py
+++ superphy/src/app/app/__init__.py
@@ -33,16 +33,7 @@ def create_app(config_name):
     if not app.debug and not app.testing and not app.config['SSL_DISABLE']:
         from flask.ext.sslify import SSLify
         sslify = SSLify(app)
-
-    from .main import main as main_blueprint
-    app.register_blueprint(main_blueprint)
-
-    from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint, url_prefix='/auth')
-
-    from .api_1_0 import api as api_1_0_blueprint
-    app.register_blueprint(api_1_0_blueprint, url_prefix='/api/v1.0')
-
+        
     from .mithril_endpoint import mithril as mithril_blueprint
     app.register_blueprint(mithril_blueprint, url_prefix='/mithril')
 
