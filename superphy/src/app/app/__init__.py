ed0d332c276a9b9f991d59bb6eef4348b6a44db4
Bryce Drew <BryceDrew.1@gmail.com>
2016-03-15 15:16:20 -0600

    users can be added to sqlite database. Urls can be restricted to users

--- superphy/src/app/app/__init__.py
+++ superphy/src/app/app/__init__.py
@@ -1,26 +1,47 @@
+"""
+__init__.py
+
+app for the flask server
+
+"""
+
+import os
+
 from flask import Flask
 
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.httpauth import HTTPBasicAuth
 
-from config import config
+from .config import config
+
+
 
 db = SQLAlchemy()
+auth = HTTPBasicAuth()
+
+
+from .models import User
 
 def create_app(config_name):
+    """
+    creates the application from the aggregation of blueprints
+    """
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
 
     db.init_app(app)
 
-    #if not app.debug and not app.testing and not app.config['SSL_DISABLE']:
-    #    from flask.ext.sslify import SSLify
-    #    sslify = SSLify(app)
-        
+    with app.app_context():
+        db.create_all()
+
+    from.example import example as example_blueprint
+    app.register_blueprint(example_blueprint, url_prefix='/example')
+
     from .data import data as data_blueprint
     app.register_blueprint(data_blueprint, url_prefix='/data')
 
-    from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint, url_prefix='/auth')
-     
+    from .api import api as api_blueprint
+    app.register_blueprint(api_blueprint, url_prefix='/api')
+
     return app
