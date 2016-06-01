96b0bb8f97bab21faea5010be20f05fb812a5253
Bryce Drew <BryceDrew.1@gmail.com>
2016-06-01 12:12:19 -0600

    basic blueprints

--- var_www_SuperPhy/SuperPhy/__init__.py
+++ var_www_SuperPhy/SuperPhy/__init__.py
@@ -2,11 +2,12 @@
 import os
 from flask import Flask, render_template, jsonify
 
+from SuperPhy.blueprints.simple import simple as simple_blueprint
+
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-	print os.path.abspath(os.path.dirname(__file__))
 	return render_template('simple_index.html')
 
 @app.route("/Info")
@@ -22,5 +23,24 @@ def routes():
 def exception_handler(error):
     return jsonify({"ERROR": repr(error)})
 
+app.register_blueprint(simple_blueprint, url_prefix='/simple')
+
+
 if __name__ == "__main__":
     app.run()
+
+"""
+    from.example import example as example_blueprint
+    app.register_blueprint(example_blueprint, url_prefix='/example')
+
+    from .data import data as data_blueprint
+    app.register_blueprint(data_blueprint, url_prefix='/data')
+
+    from .api import api as api_blueprint
+    app.register_blueprint(api_blueprint, url_prefix='/api')
+
+    from .upload import upload as upload_blueprint
+    app.register_blueprint(upload_blueprint, url_prefix='/upload')
+
+    return app
+"""
---
+++ var_www_SuperPhy/SuperPhy/blueprints/simple.py
@@ -0,0 +1,21 @@
+import os
+from flask import request, jsonify, url_for, Blueprint
+
+simple = Blueprint('simple', __name__)
+
+@simple.after_request
+def add_header(response):
+    """
+    Append after request the nessesary headers.
+    """
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Headers'] = "accept, content-type"
+    return response
+
+@simple.route('/foo')
+def foo():
+    return jsonify({'foo':url_for('simple.bar')})
+
+@simple.route('/bar')
+def bar():
+    return jsonify({'bar':'bar'})
