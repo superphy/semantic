02bcfeb6e51d314c07ee808dbf92b6141b5596a8
Bryce Drew <BryceDrew.1@gmail.com>
2016-06-01 11:30:20 -0600

    added in dev page for listing all routes.

--- var_www_SuperPhy/SuperPhy/__init__.py
+++ var_www_SuperPhy/SuperPhy/__init__.py
@@ -1,15 +1,22 @@
-from flask import Flask, render_template, jsonify, json
+
 import os
+from flask import Flask, render_template, jsonify
+
 app = Flask(__name__)
 
-@app.route("/Foo")
-def hello():
-	return jsonify({"HELLO":"WORLD"})
-
 @app.route("/")
-def foo():
+def index():
 	print os.path.abspath(os.path.dirname(__file__))
 	return render_template('index.html')
+
+@app.route("/Info")
+def routes():
+    routes = []
+    for rule in app.url_map.iter_rules():
+        if "GET" in rule.methods:
+            routes.append(rule.rule)
+    routes.sort()
+    return render_template('routes.html', routes=sorted(routes, key=lambda s: s.lower()))
 
 @app.errorhandler(Exception)
 def exception_handler(error):
