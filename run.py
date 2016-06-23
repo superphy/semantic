8b177a9c2ee2eb8734fac100010d87c3b175812a
Bryce Drew <BryceDrew.1@gmail.com>
2016-06-23 15:26:19 -0600

    added in environ variable support. Moved static constants out of install

--- run.py
+++ run.py
@@ -26,6 +26,17 @@ def run(args):
         #   security hazard if you allow port 5000 traffic.
         subprocess.call("/usr/bin/python var_www_SuperPhy/run.py", shell=True)
 
+
+
+GIT_CONF_FILE = "development_virtualhost.conf"
+APACHE = "apache2"
+APACHE_CONF_DESTINATION = "/etc/{apache}/sites-available".format(apache=APACHE)
+APACHE_SYMLINK_DESTINATION = '/var/www/SuperPhy'
+NETWORK_FACEING_SUBDIRECTORY = 'var_www_SuperPhy'
+VIRTUALENV_FOLDER = "venv"
+NPM_REQUIRMENTS = "npm-requirements.txt"
+BOWER_COMPONENTS_DESTINATION = "var_www_SuperPhy/SuperPhy/static/js/bower_components/mithril-components"
+
 def install(args):
     """
     Download, install, initialize, etc.
@@ -34,53 +45,50 @@ def install(args):
     """
     if args.sys:
         args.symlink = True
-        #Setup fresh image to use SuperPhy
-
+        #Connect this repo with apache
         subprocess.call("sudo a2enmod wsgi", shell=True)
-        subprocess.call("sudo cp $(pwd)/development_virtualhost.conf /etc/apache2/sites-available/000-default.conf", shell=True)
-        subprocess.call("sudo service apache2 reload", shell=True)
+        subprocess.call("sudo cp $(pwd)/{conf} {conf_destination}/000-default.conf".format(conf=GIT_CONF_FILE, conf_destination=APACHE_CONF_DESTINATION), shell=True)
+        subprocess.call("sudo service {apache} reload".format(apache=APACHE), shell=True)
 
-        #This is a very large download. If you aren't going to be uploading data, don't bother downloading it.
-        #subprocess.call("mkdir blast", shell=True)
-        #subprocess.call("if ! find blast/ncbi*/ | read v; then cd blast; wget -rc -nd -A "*x64-linux.tar.gz" "ftp://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/LATEST/"; tar zxvpf *x64-linux.tar.gz; cd ..; fi", shell=True)
+    #This is a very large download. If you aren't going to be uploading data, don't bother downloading it.
+    #subprocess.call("mkdir blast", shell=True)
+    #subprocess.call("if ! find blast/ncbi*/ | read v; then cd blast; wget -rc -nd -A "*x64-linux.tar.gz" "ftp://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/LATEST/"; tar zxvpf *x64-linux.tar.gz; cd ..; fi", shell=True
 
     if args.symlink:
         if args.verbose:
             print "SYMLINKING"
-        apache = os.path.join('/var/www/', 'SuperPhy')
-        if args.verbose:
-            print "APACHE: %s" %apache
-        project = os.path.join(os.getcwd(), 'var_www_SuperPhy')
-        subprocess.call("sudo rm -f %s" % apache, shell=True)
-        subprocess.call("sudo ln -s %s %s" % (project, apache), shell=True)
+        subprocess.call("sudo rm -f {0}".format(APACHE_SYMLINK_DESTINATION), shell=True)
+        subprocess.call("sudo ln -s {0} {1}".format(os.path.join(os.getcwd(), NETWORK_FACEING_SUBDIRECTORY), APACHE_SYMLINK_DESTINATION), shell=True)
 
     #Install virtualenv binaries
 
     if args.verbose:
         print "Initializing virtualenv"
-    subprocess.call("virtualenv --no-site-packages venv", shell=True)
+    subprocess.call("virtualenv --no-site-packages {venv}".format(venv=VIRTUALENV_FOLDER), shell=True)
 
     if args.verbose:
-        print "Upgrading pip"
-    subprocess.call("venv/bin/pip install --upgrade pip", shell=True)
+        print "Upgrading virtualenv pip"
+    subprocess.call("{venv}/bin/pip install --upgrade pip".format(venv=VIRTUALENV_FOLDER), shell=True)
 
     if args.verbose:
-        print "Installing Pip requirments into virtualenv"
-    subprocess.call("venv/bin/pip install -r venv/requirements.txt", shell=True)
+        print "Installing Pip requirments into {venv}".format(venv=VIRTUALENV_FOLDER)
+    subprocess.call("{venv}/bin/pip install -r venv/requirements.txt".format(venv=VIRTUALENV_FOLDER), shell=True)
 
     if args.verbose:
         print "Installing Npm requirments into virtualenv"
-    subprocess.call("cd venv/lib; ../bin/nodeenv -p --prebuilt --requirements=../npm-requirements.txt", shell=True)
+    subprocess.call("cd {venv}/lib; ../bin/nodeenv -p --prebuilt --requirements=../{npm}".format(npm=NPM_REQUIRMENTS, venv=VIRTUALENV_FOLDER), shell=True)
 
+
+    #Refactor This into a submodule of our project?
     if args.verbose:
         print "Grabbing mithril components from github"
-    subprocess.call("git clone --depth=1 https://github.com/eddyystop/mithril-components.git var_www_SuperPhy/SuperPhy/static/js/bower_components/mithril-components;", shell=True)
+    subprocess.call("git clone --depth=1 https://github.com/eddyystop/mithril-components.git {dest};".format(dest=BOWER_COMPONENTS_DESTINATION), shell=True)
 
     #For some reason, even apache doesn't work if you don't run the pyserver beforehand.
     if args.verbose:
         print "Restarting and running pyserver. (Press Ctrl-c to exit):"
 
-    subprocess.call("python run.py run --pyserver {0}".format("--verbose" if args.verbose else ""), shell=True)
+    subprocess.call("python {this} run --pyserver {verbose}".format(this=__file__, verbose="--verbose" if args.verbose else ""), shell=True)
 
 
 def pull(args):
@@ -137,6 +145,51 @@ def parse_args():
 
     return args
 
+def import_env(verbose=True):
+    """
+    Import Environment Variables from configuration file.
+     for example: [rdf] url = localhost/blazegraph/namespace/superphy/sparql
+     becomes:     ['SUPERPHY_RDF_URL', ' localhost/blazegraph/namespace/superphy/sparql']
+    """
+    #Create active configuration file.
+    if not os.path.exists("config/active.cfg"):
+        #Copy of config file from default
+        subprocess.call("cp config/default.cfg config/active.cfg", shell=True)
+    #Setting the Environment Variables
+    for line in open('config/superphy.cfg'):
+        #Ignore lines that are commented out.
+        if line.startswith("#"):
+            continue
+        #Seperate and Filter the line into a key-value pair
+        key, value = line.strip().split('=')
+
+        #Sort the key into each tag
+        key = key.strip().translate(None, '[ ').split(']')
+
+        #Add the header 'SUPERPHY' to the front of all keys
+        if key[0] is not "SUPERPHY":
+            key.insert(0, "SUPERPHY")
+
+        #Aggregate the keys into one string.
+        for i, tag in enumerate(key):
+            key[i] = tag.upper()
+        key = '_'.join(key)
+
+        #Remove Whitespace from value
+        value = value.strip()
+
+        #Set the environment variables.
+        os.environ[key] = value
+
+    #Show all environment varaibles
+    if verbose:
+        for key in os.environ.keys():
+            if "SUPERPHY" in key:
+                print key, os.environ[key]
+
+
+
+
 if __name__ == "__main__":
 
     OPTIONS = {
@@ -147,4 +200,8 @@ if __name__ == "__main__":
     }
 
     ARGS = parse_args()
+    import_env(ARGS.verbose)
     OPTIONS[ARGS.method](ARGS)
+    
+
+
