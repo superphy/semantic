35c7202bf73c52e658805f7b66a381acb5ef96b6
Chad Laing <chadlaing@gmail.com>
2016-07-12 10:11:54 -0600

    start of example

---
+++ app/SuperPhy/models/upload/example_json.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+from SuperPhy.models.sparql.genomes import get_genome_metadata
+
+
+"""
+Example using the _sparql functions for returning data
+"""
+
