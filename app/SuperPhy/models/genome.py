950979ced3b0120656a47446c4b20fd904fa0a6a
Bryce Drew <BryceDrew.1@gmail.com>
2016-07-14 11:59:46 -0600

    starting a simple genome model

---
+++ app/SuperPhy/models/genome.py
@@ -0,0 +1,16 @@
+import rdflib
+import os
+import requests
+
+from SuperPhy.models.upload.classes.namespaces import n, owl, rdf, xml, xsd, rdfs, gfvo, faldo
+
+class Genome(object):
+    """
+    .
+    """
+    def __init__(self):
+        """
+        .
+        """
+        self.data = rdflib.Graph()
+
