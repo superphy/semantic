ad838e0d73531674f5bffda72a60d61d068b99a9
Bryce Drew <BryceDrew.1@gmail.com>
2016-07-14 12:01:21 -0600

    some meta-data parsing

--- app/SuperPhy/models/genome.py
+++ app/SuperPhy/models/genome.py
@@ -30,3 +30,36 @@ class Genome(object):
         )
         return request.content
 
+    def add_metadata(self, metadata):
+
+        #Add literal values to the graph
+        has_values = {
+            "Accession": n.has_accession,
+            "Bioproject_Id": n.has_bioproject,
+            "Biosample_Id": n.has_biosample,
+            "Serotype_H": n.has_Htype,
+            "Serotype_O": n.has_Otype,
+            "Strain": n.has_strain,
+            "Isolation_date": n.has_isolation_date,
+            "Isolation_location": n.has_geographic_location,
+        }
+
+        for key, predicate in has_values.items():
+            if key in metadata:
+                self.data.add((
+                    n[metadata[key]], predicate, rdflib.Literal(metadata[key], datatype=rdflib.XSD.string)
+                ))
+
+        #Set mandatory links
+        self.data.add()
+
+genome = Genome()
+genome.add_metadata({
+    "Accession":"AAJW00000000",
+    "Bioproject_Id":"15578",
+    "Biosample_Id":"2435896",
+    "Serotype_H":"9",
+    "Serotype_O":"111",
+    "Strain":"E110019"
+})
+print genome.data.serialize(format="turtle")
