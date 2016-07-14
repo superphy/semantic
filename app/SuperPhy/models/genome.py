262113f3d5f6ae62dff3a36041192db7a6615b1b
Bryce Drew <BryceDrew.1@gmail.com>
2016-07-14 12:26:33 -0600

    refactored metadata import & Changed class to plural genomes.

--- app/SuperPhy/models/genome.py
+++ app/SuperPhy/models/genome.py
@@ -4,7 +4,7 @@ import requests
 
 from SuperPhy.models.upload.classes.namespaces import n, owl, rdf, xml, xsd, rdfs, gfvo, faldo
 
-class Genome(object):
+class Genomes(object):
     """
     .
     """
@@ -31,30 +31,65 @@ class Genome(object):
         return request.content
 
     def add_metadata(self, metadata):
+        """
+        This method is to add data fields to the rdflib Graph object.
 
-        #Add literal values to the graph
-        has_values = {
-            "Accession": n.has_accession,
-            "Bioproject_Id": n.has_bioproject,
-            "Biosample_Id": n.has_biosample,
-            "Serotype_H": n.has_Htype,
-            "Serotype_O": n.has_Otype,
-            "Strain": n.has_strain,
-            "Isolation_date": n.has_isolation_date,
-            "Isolation_location": n.has_geographic_location,
-        }
+        Inputs: A simple key-value dictionary containing values for
+        Changes: The graph object will have triples added to it.
+        Outputs: None
+        """
+        #'accession', 'bioproject', 'biosample', 'htype', 'isolation_date', 'isolation_host', 'isolation_location', 'isolation_source', 'organism', 'otype', 'strain', 'syndrome'
 
-        for key, predicate in has_values.items():
-            if key in metadata:
-                self.data.add((
-                    n[metadata[key]], predicate, rdflib.Literal(metadata[key], datatype=rdflib.XSD.string)
-                ))
+        if "Accession" in metadata:
+            self.data.add((
+                n[metadata["Accession"]], n.has_accession,\
+                rdflib.Literal(metadata["Accession"], datatype=rdflib.XSD.string)
+            ))
 
-        #Set mandatory links
-        self.data.add()
+        if "Bioproject_Id" in metadata:
+            self.data.add((
+                n[metadata["Bioproject_Id"]], n.has_bioproject,\
+                rdflib.Literal(metadata["Bioproject_Id"], datatype=rdflib.XSD.string)
+            ))
+            
+        if "Biosample_Id" in metadata:
+            self.data.add((
+                n[metadata["Biosample_Id"]], n.has_biosample,\
+                rdflib.Literal(metadata["Biosample_Id"], datatype=rdflib.XSD.string)
+            ))
 
-genome = Genome()
-genome.add_metadata({
+        if "Serotype_H" in metadata:
+            self.data.add((
+                n[metadata["Serotype_H"]], n.has_Htype,\
+                rdflib.Literal(metadata["Serotype_H"], datatype=rdflib.XSD.string)
+            ))
+
+        if "Serotype_O" in metadata:
+            self.data.add((
+                n[metadata["Serotype_O"]], n.has_Otype,\
+                rdflib.Literal(metadata["Serotype_O"], datatype=rdflib.XSD.string)
+            ))
+
+        if "Strain" in metadata:
+            self.data.add((
+                n[metadata["Strain"]], n.has_strain,\
+                rdflib.Literal(metadata["Strain"], datatype=rdflib.XSD.string)
+            ))
+
+        if "Isolation_date" in metadata:
+            self.data.add((
+                n[metadata["Isolation_date"]], n.has_isolation_date,\
+                rdflib.Literal(metadata["Isolation_date"], datatype=rdflib.XSD.string)
+            ))
+
+        if "Isolation_location" in metadata:
+            self.data.add((
+                n[metadata["Isolation_location"]], n.has_geographic_location,\
+                rdflib.Literal(metadata["Isolation_location"], datatype=rdflib.XSD.string)
+            ))
+
+genomes = Genomes()
+genomes.add_metadata({
     "Accession":"AAJW00000000",
     "Bioproject_Id":"15578",
     "Biosample_Id":"2435896",
@@ -62,4 +97,4 @@ genome.add_metadata({
     "Serotype_O":"111",
     "Strain":"E110019"
 })
-print genome.data.serialize(format="turtle")
+print genomes.data.serialize(format="turtle")
