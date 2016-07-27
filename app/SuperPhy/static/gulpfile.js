23e4670a08861812b1241cdc92a3f1c883cb85aa
mwhitesi <mdwhitsi@gmail.com>
2016-07-27 17:18:53 -0600

    Added browserify to gulpfile

--- app/SuperPhy/static/gulpfile.js
+++ app/SuperPhy/static/gulpfile.js
@@ -12,6 +12,11 @@ var concat = require('gulp-concat');
 var flatten = require('gulp-flatten');
 var order = require('gulp-order');
 var print = require('gulp-print');
+var browserify  = require('browserify');
+var source      = require('vinyl-source-stream'); //to 'rename' your resulting file
+var buffer      = require('vinyl-buffer'); // to transform the browser
+var sourcemaps  = require('gulp-sourcemaps');
+var coffeeify  = require('coffeeify');
 
 var ordering = [
   "__init__.coffee",
@@ -23,6 +28,39 @@ var ordering = [
   "**/*.coffee",
   "route.coffee"
 ];
+
+var config = {
+    coffee: {
+        src: 'CoffeeScript/delete_later.coffee',       // Entry point
+        outputDir: './js/',  // Directory to save bundle to
+        mapDir: './maps/',      // Subdirectory to save maps to
+        outputFile: 'superphy_bundle.js' // Name to use for bundle
+    },
+};
+
+// This method makes it easy to use common bundling options in different tasks
+function bundle (bundler) {
+
+    // Add options to add to "base" bundler passed as parameter
+    bundler
+      .bundle()                                                        // Start bundle
+      .pipe(source(config.coffee.src))                                 // Entry point
+      .pipe(buffer())                                                  // Convert to gulp pipeline
+      .pipe(rename(config.coffee.outputFile))                          // Rename output
+      .pipe(sourcemaps.init({ loadMaps : true, debug: true }))  // Strip inline source maps
+      .pipe(uglify())  // Minify
+      .pipe(sourcemaps.write(config.coffee.mapDir))    // Save source maps to own directory
+      .pipe(gulp.dest(config.coffee.outputDir))        // Save 'bundle'
+      //.pipe(livereload());                                       // Reload browser if relevant
+}
+
+gulp.task('bundle', function () {
+    var bundler = browserify(config.coffee.src)  // Pass browserify the entry point
+                    .transform(coffeeify)      //  Chain transformations: First, coffeeify . . .
+                              
+
+    bundle(bundler);  // Chain other options -- sourcemaps, rename, etc.
+})
 
 gulp.task('coffee_to_js',  function() {
 	gulp
