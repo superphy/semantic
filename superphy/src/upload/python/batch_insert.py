14979221c37f13b797393383a557888acc6f4b30
Kevin Le <kevin.kent.le@gmail.com>
2017-01-23 16:53:14 -0700

    still off by 1, about to add ectyper as submodule

--- superphy/src/upload/python/batch_insert.py
+++ superphy/src/upload/python/batch_insert.py
@@ -4,12 +4,18 @@ import subprocess
 
 from multiprocessing import Pool, cpu_count
 from os import listdir
+from time import time
 
 def batch_call(filename):
     subprocess.call(['./insert.py', '-i', 'tmp/' + filename])
 if __name__ == "__main__":
 
+    start = time()
+    print 'Starting batch insert at: ', start
+
     p = Pool(cpu_count())
     p.map(batch_call, listdir('tmp'))
 
     print '***ALL DONE***'
+    print 'Completed at: ', time()
+    print 'Elapsed: ', time() - s
