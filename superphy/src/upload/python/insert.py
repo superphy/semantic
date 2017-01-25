160265f82c8f7a1207b5e345ebae035aeae9c71f
Kevin Le <kevin.kent.le@gmail.com>
2017-01-25 16:33:05 -0700

    should run clean now

--- superphy/src/upload/python/insert.py
+++ superphy/src/upload/python/insert.py
@@ -186,7 +186,7 @@ def generate_amr(graph, uriIsolate, fasta_file):
 
     #the rgi_json call in rgitool.py isn't needed for us
     #this generates the .tsv we want
-    subprocess.call(['rgi_tab',
+    subprocess.call(['rgi_jsontab',
         '-i', 'outputs/' + outputname + '.json',
         '-o', 'outputs/' + outputname])
 
