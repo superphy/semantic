c6d1ecd4efae65ce6dbfabab10a44f175641d30f
Kevin Le <kevin.kent.le@gmail.com>
2017-01-26 15:13:38 -0700

    typo

--- superphy/src/upload/python/insert.py
+++ superphy/src/upload/python/insert.py
@@ -210,40 +210,40 @@ def generate_amr(graph, uriIsolate, fasta_file):
 
         uriGenes = gu(uriContig, '/genes')
         #we may end up adding the same bag if the contigs are the same - it doesn't really matter, graph.add will check for this
-        graph.add(uriContig, gu('faldo:BagOfRegions'), uriGenes)
+        graph.add((uriContig, gu('faldo:BagOfRegions'), uriGenes))
 
         uriGene = gu(uriGenes, + '/' + orf_id)
-        graph.add(uriGenes, gu('so:Gene'), uriGene)
-        graph.add(uriGene, gu('g:Identifier'), gu(':' + amr_results['Best_Hit_ARO'][i])) #ex. :metN
-        graph.add(uriGene, gu('dc:Description'), amr_results['CUT_OFF'][i])
+        graph.add((uriGenes, gu('so:Gene'), uriGene))
+        graph.add((uriGene, gu('g:Identifier'), gu(':' + amr_results['Best_Hit_ARO'][i]))) #ex. :metN
+        graph.add((uriGene, gu('dc:Description'), amr_results['CUT_OFF'][i]))
 
         gene_location = BNode()
-        graph.add(uriGene, gu('faldo:location'), gene_location)
-        graph.add(gene_location, gu('rdf:type'), gu('faldo:Region')) #rdf:type is same as 'a'
+        graph.add((uriGene, gu('faldo:location'), gene_location))
+        graph.add((gene_location, gu('rdf:type'), gu('faldo:Region'))) #rdf:type is same as 'a'
 
         gene_start = BNode()
         gene_end = BNode()
 
-        graph.add(gene_location, gu('faldo:Begin'), gene_start)
-        graph.add(gene_start, gu('rdf:type'), gu('faldo:Position'))
-        graph.add(gene_start, gu('rdf:type'), gu('faldo:ExactPosition'))
+        graph.add((gene_location, gu('faldo:Begin'), gene_start))
+        graph.add((gene_start, gu('rdf:type'), gu('faldo:Position')))
+        graph.add((gene_start, gu('rdf:type'), gu('faldo:ExactPosition')))
 
-        graph.add(gene_location, gu('faldo:End'), gene_end)
-        graph.add(gene_end, gu('rdf:type'), gu('faldo:Position'))
-        graph.add(gene_end, gu('rdf:type'), gu('faldo:ExactPosition'))
+        graph.add((gene_location, gu('faldo:End'), gene_end))
+        graph.add((gene_end, gu('rdf:type'), gu('faldo:Position')))
+        graph.add((gene_end, gu('rdf:type'), gu('faldo:ExactPosition')))
 
         if amr_results['ORIENTATION'][i] is '+':
-            graph.add(gene_start, gu('rdf:type'), gu('faldo:ForwardStrandPosition'))
-            graph.add(gene_end, gu('rdf:type'), gu('faldo:ForwardStrandPosition'))
+            graph.add((gene_start, gu('rdf:type'), gu('faldo:ForwardStrandPosition')))
+            graph.add((gene_end, gu('rdf:type'), gu('faldo:ForwardStrandPosition')))
         else:
-            graph.add(gene_start, gu('rdf:type'), gu('faldo:ReverseStrandPosition'))
-            graph.add(gene_end, gu('rdf:type'), gu('faldo:ReverseStrandPosition'))
+            graph.add((gene_start, gu('rdf:type'), gu('faldo:ReverseStrandPosition')))
+            graph.add((gene_end, gu('rdf:type'), gu('faldo:ReverseStrandPosition')))
 
-        graph.add(gene_start, gu('faldo:Position'), Literal(amr_results['START'][i]))
-        graph.add(gene_start, gu('faldo:Reference'), uriContig)
+        graph.add((gene_start, gu('faldo:Position'), Literal(amr_results['START'][i])))
+        graph.add((gene_start, gu('faldo:Reference'), uriContig))
 
-        graph.add(gene_end, gu('faldo:Position'), Literal(amr_results['STOP'][i]))
-        graph.add(gene_end, gu('faldo:Reference'), uriContig)
+        graph.add((gene_end, gu('faldo:Position'), Literal(amr_results['STOP'][i])))
+        graph.add((gene_end, gu('faldo:Reference'), uriContig))
 
     return graph
 
