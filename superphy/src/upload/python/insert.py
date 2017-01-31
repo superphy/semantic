0f891729d1eb8a33f655736a032b859ea31722d1
Kevin Le <kevin.kent.le@gmail.com>
2017-01-31 14:52:04 -0700

    making amr dict

--- superphy/src/upload/python/insert.py
+++ superphy/src/upload/python/insert.py
@@ -267,6 +267,10 @@ def generate_amr(graph, uriIsolate, fasta_file):
     amr_results = amr_results[
         ['ORF_ID', 'START', 'STOP', 'ORIENTATION', 'CUT_OFF', 'Best_Hit_ARO']]
 
+    amr_results.rename(columns={'ORF_ID':'contig_id','Best_Hit_ARO':'GENE_NAME'}, inplace=True)
+
+    amr_results['contig_id'].apply(lambda n: n.strip().split('_')[-1])[0].split('_')[0])
+
     # triple generation
     for i in amr_results.index:
 
