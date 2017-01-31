1545dbebd8f37749e0762e87b0a063e7634b94ff
Kevin Le <kevin.kent.le@gmail.com>
2017-01-31 14:57:39 -0700

    hopefully a lil more human readable

--- superphy/src/upload/python/insert.py
+++ superphy/src/upload/python/insert.py
@@ -269,7 +269,8 @@ def generate_amr(graph, uriIsolate, fasta_file):
 
     amr_results.rename(columns={'ORF_ID':'contig_id','Best_Hit_ARO':'GENE_NAME'}, inplace=True)
 
-    amr_results['contig_id'].apply(lambda n: n.strip().split(n.split('_')[-1])[0].split('_')[0])
+    amr_results['contig_id'].apply(lambda n: n.strip())
+    amr_results['contig_id'].apply(lambda n: n.split(n.split('_')[-1])[0].split('_')[0])
 
     # triple generation
     for i in amr_results.index:
