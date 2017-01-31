54fabeb84c24f5deeab9722f01a6c7ca7037d3bd
Kevin Le <kevin.kent.le@gmail.com>
2017-01-31 15:08:16 -0700

    testing

--- superphy/src/upload/python/insert.py
+++ superphy/src/upload/python/insert.py
@@ -269,7 +269,10 @@ def generate_amr(graph, uriIsolate, fasta_file):
 
     amr_results.rename(columns={'ORF_ID':'contig_id','Best_Hit_ARO':'GENE_NAME'}, inplace=True)
 
-    amr_results['contig_id'].apply(lambda n: n.strip().rsplit('_',1)[0])
+    # sometimes there are spaces, also we remove the additional occurance tag that RGI adds to contig ids
+    print amr_results['contig_id'].apply(lambda n: n.strip().rsplit('_',1)[0])
+
+    amr_results['contig_id'] = amr_results['contig_id'].apply(lambda n: n.strip().rsplit('_',1)[0])
 
     print amr_results
 
