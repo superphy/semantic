4747694351822e6271232d17d72ecc8f3f5ae52a
Kevin Le <kevin.kent.le@gmail.com>
2017-01-31 15:39:51 -0700

    all good!!

--- superphy/src/upload/python/insert.py
+++ superphy/src/upload/python/insert.py
@@ -274,16 +274,7 @@ def generate_amr(graph, uriIsolate, fasta_file):
     amr_results['contig_id'] = amr_results['contig_id'].apply(
         lambda n: n.strip().rsplit('_', 1)[0])
 
-    '''
-    # TODO: write a faster way of doing the same thing
-    amr_dict = {}
-    for i in amr_results.index:
-        contig_id = amr_results['contig_id'][i]
-        if contig_id not in amr_dict.keys():
-            amr_dict[contig_id] = []
-        amr_dict[contig_id].append(amr_results[['START', 'STOP', 'ORIENTATION', 'CUT_OFF', 'GENE_NAME']][i])
-    '''
-    amr_dict = amr_results.set_index('contig_id').to_dict(orient='index')
+    amr_results = amr_results.set_index('contig_id').to_dict(orient='index')
 
     print amr_dict
 
