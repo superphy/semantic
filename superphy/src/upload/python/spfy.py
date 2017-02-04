4115e1c83aca13494d10070ddc6b89e19e055592
Kevin Le <kevin.kent.le@gmail.com>
2017-02-03 19:25:45 -0700

    typo

--- superphy/src/upload/python/spfy.py
+++ superphy/src/upload/python/spfy.py
@@ -30,9 +30,8 @@ def blob_savvy(args_dict):
     if os.path.isdir(args_dict['i']):
         for f in os.listdir(args_dict['i']):
             single_dict=dict(args_dict.items() + {'uriIsolate':args_dict['uris'][f]['uriIsolate'], 'uriGenome':args_dict['uris'][f]['uriGenome'], 'i':args_dict[i]+'/'+f}.items())
-            #high.enqueue(savvy, dict(single_dict.items() + {'disable_amr': True}.items()))
-            #low.enqueue(savvy, dict(single_dict.items() + {'disable_vf':True,'disable_serotype':True}.items()))
-            low.enqueue(savvy,single_dict)
+            high.enqueue(savvy, dict(single_dict.items() + {'disable_amr': True}.items()))
+            low.enqueue(savvy, dict(single_dict.items() + {'disable_vf':True,'disable_serotype':True}.items()))
     else:
         # run the much faster vf and serotyping separately of amr
         high.enqueue(savvy, dict(args_dict.items() + {'disable_amr': True}.items()))
