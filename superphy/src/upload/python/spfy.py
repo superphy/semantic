6270e1718fbed87ffa7c9c329334152ab351e1fc
Kevin Le <kevin.kent.le@gmail.com>
2017-02-03 19:20:54 -0700

    typo

--- superphy/src/upload/python/spfy.py
+++ superphy/src/upload/python/spfy.py
@@ -29,13 +29,13 @@ def blob_savvy(args_dict):
     '''
     if os.path.isdir(args_dict['i']):
         for f in os.listdir(args_dict['i']):
-            single_dict=dict(args_dict.items() + {'uriIsolate':args_dict['uris'][f]['uriIsolate'], 'uriGenome':args_dict['uris'][f]['uriGenome'], 'i':args_dict[i]+'/'+f})
-            high.enqueue(savvy, dict(single_dict.items() + {'disable_amr': True}))
-            low.enqueue(savvy, dict(single_dict.items() + {'disable_vf':True,'disable_serotype':True}))
+            single_dict=dict(args_dict.items() + {'uriIsolate':args_dict['uris'][f]['uriIsolate'], 'uriGenome':args_dict['uris'][f]['uriGenome'], 'i':args_dict[i]+'/'+f}.items())
+            high.enqueue(savvy, dict(single_dict.items() + {'disable_amr': True}.items()))
+            low.enqueue(savvy, dict(single_dict.items() + {'disable_vf':True,'disable_serotype':True}.items()))
     else:
         # run the much faster vf and serotyping separately of amr
-        high.enqueue(savvy, dict(args_dict.items() + {'disable_amr': True}))
-        low.enqueue(savvy, dict(args_dict.items() + {'disable_vf':True,'disable_serotype':True}))
+        high.enqueue(savvy, dict(args_dict.items() + {'disable_amr': True}.items()))
+        low.enqueue(savvy, dict(args_dict.items() + {'disable_vf':True,'disable_serotype':True}.items()))
 
 def monitor():
     '''
